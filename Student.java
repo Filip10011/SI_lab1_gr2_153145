215ece3c8b1805759cf965d8d0cc9ba556378b93
Filip <popovski_f@hotmail.com>
2020-04-07 23:10:43 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -3,6 +3,13 @@ class Student {
 	String firstName;
 	String lastName;
 	int grades[];
+	int num1 = 6;
+	int num2 = 7;
+	int num3 = 8;
+	int num4 = 7;
+	int num5 = 10;
+	string polozen;
+	int br;
 
 	//TODO constructor
 
@@ -10,10 +17,25 @@ class Student {
 
 	public double getAverage() {
 		//TODO
+		int average = (num1 + num2 + num3 + num4 + num5) / 5);
+		System.out.println(average);
 		
 	}
 
 	public int ECTSCredits() {
 		//TODO
+		if(num1 == polozen) {
+			br++;
+		}else if(num2 == polozen) {
+			br++;
+		}else if(num3 == polozen) {
+			br++;
+		}else if(num4 == polozen) {
+			br++;
+		}else if(num5 == polozen) {
+			br++;	
+		}else(){
+			System.out.println("nema bed");
+		}
 	}
 }
